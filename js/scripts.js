c67cd153abbce94074e63a9ac7e3da63d983fcc5
Devin Mounts <devinmounts@gmail.com>
2018-06-01 10:15:09 -0700

    add functionality using javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,33 @@
 $(function(){
   $("#submitButton").click(function(event){
-    ()
+    var questionOne = parseInt($("#firstQuestion").val());
+
+    var questionTwo = parseInt($("#secondQuestion").val());
+
+    var questionThree = parseInt($("#thirdQuestion").val());
+
+    var questionFour = parseInt($("#fourthQuestion").val());
+
+    var questionFive = parseInt($("#fifthQuestion").val());
+
+    var questionSix = parseInt($("#sixthQuestion").val());
+
+    $(".track-match").hide();
+
+    var total = questionOne + questionTwo + questionThree + questionFour + questionFive + questionSix;
+    alert(total);
+
+    if(total <= 6){
+      $("#cSharp").show();
+    } else if(total <=12 && total >=7){
+      $("#react").show();
+    } else if(total >=13 && total <=18){
+      $("#ruby").show();
+    } else {
+      $("#research").show();
+    }
+
+
 
   event.preventDefault();
   });
