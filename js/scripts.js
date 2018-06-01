2e246d6c1d9c2bf4c99602d2da2d6ec24db256bd
Devin Mounts <devinmounts@gmail.com>
2018-06-01 16:06:31 -0700

    add styling to response images.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,19 @@
 $(function(){
   $("#submitButton").click(function(event){
     var questionOne = $("#firstQuestion").val();
-    alert(questionOne);
-
     var questionTwo = $("#secondQuestion").val();
-    alert(questionTwo);
     var questionThree = $("#thirdQuestion").val();
-
     var questionFour = $("#fourthQuestion").val();
-
     var questionFive = $("#fifthQuestion").val();
-    alert(questionFive);
 
     $(".track-match").hide();
 
-
     var numLine = questionOne + questionTwo + questionThree + questionFour + questionFive;
-    alert(numLine);
 
     var count1 = (numLine.match(/1/g) || []).length;
     var count2 = (numLine.match(/2/g) || []).length;
     var count3 = (numLine.match(/3/g) || []).length;
     var count4 = (numLine.match(/4/g) || []).length;
-    alert(count4);
 
     if(count1>count2 && count1>count3 && count1>count4){
       $("#cSharp").show();
@@ -33,11 +24,6 @@ $(function(){
     } else {
       $("#research").show();
     }
-
-
-
   event.preventDefault();
   });
-
-
 });
